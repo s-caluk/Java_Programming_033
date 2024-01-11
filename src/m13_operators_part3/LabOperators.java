bf10607e4d6e4e272ba03e21f9ac7c61f026711a
s-caluk <seher.caluk@gmail.com>
2024-01-11 12:49:57 +0100

    V2

--- src/m13_operators_part3/LabOperators.java
+++ src/m13_operators_part3/LabOperators.java
@@ -39,5 +39,7 @@ public class LabOperators {
         boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
         System.out.println("Is " + year + "a leap year ? " + isLeapYear);
 
+        
+
     }
 }
